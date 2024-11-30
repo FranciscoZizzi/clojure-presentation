e39014bf4cb94bfffc90a2c169de15343d97ce28
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-30 19:43:59 -0300

    fix double scroll bar on slides

--- dev/user.clj
+++ dev/user.clj
@@ -1,10 +1,28 @@
 (ns user
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-;; Build a html file from the given notebook notebooks.
-;; See the docstring for more options.
+;; I didn't know how to change the css of the main body of the notebook
+;; So I just modify the built html with the css property that I wanted
+;; There was scroll on every slide, and it made me ANGRY
+(defn add-css-property "Takes the file path, looks for body { ... } and adds overflow: hidden" [file-path]
+  (let [html-content (slurp file-path)
+        updated-content (str/replace html-content
+                                     #"body \{([^\}]+)\}"
+                                     (fn [[full-match inner-content]]
+                                       ;; Check if "overflow: hidden;" exists in the inner content
+                                       (if (str/includes? inner-content "overflow: hidden;")
+                                         full-match
+                                         ;; Add "overflow: hidden;" before closing "}"
+                                         (str "body {" inner-content "  overflow: hidden;\n}"))))]
+    (spit file-path updated-content)))
+
 (defn -main []
-  (clerk/build! {:paths ["notebooks/slideshow.clj"]}))
+  (do
+    (clerk/build! {:paths ["notebooks/slideshow.clj"]}))
+    (add-css-property "public/build/index.html")
+    )
 
 (defn dev []
   ;; start Clerk's built-in webserver on the default port 7776, opening the browser when done
