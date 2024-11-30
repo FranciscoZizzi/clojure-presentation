fa960e3d6383d04692a8819bb105e0d11c4b6104
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-30 01:07:47 -0300

    GitHub pages workflow attempt 1

--- dev/user.clj
+++ dev/user.clj
@@ -1,18 +1,21 @@
 (ns user
   (:require [nextjournal.clerk :as clerk]))
 
-;; start Clerk's built-in webserver on the default port 7777, opening the browser when done
-(clerk/serve! {:browse true})
-
-;; either call `clerk/show!` explicitly
-(clerk/show! "notebooks/slideshow.clj")
-
-;; or let Clerk watch the given `:paths` for changes
-(clerk/serve! {:watch-paths ["notebooks" "src"]})
-
-;; start with watcher and show filter function to enable notebook pinning
-(clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
-
 ;; Build a html file from the given notebook notebooks.
 ;; See the docstring for more options.
-(clerk/build! {:paths ["notebooks/slideshow.clj"]})
+(defn -main []
+  (clerk/build! {:paths ["notebooks/slideshow.clj"]}))
+
+(defn dev []
+  ;; start Clerk's built-in webserver on the default port 7776, opening the browser when done
+  (clerk/serve! {:browse true})
+
+  ;; either call `clerk/show!` explicitly
+  (clerk/show! "notebooks/slideshow.clj")
+
+  ;; or let Clerk watch the given `:paths` for changes
+  (clerk/serve! {:watch-paths ["notebooks" "src"]})
+
+  ;; start with watcher and show filter function to enable notebook pinning
+  (clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
+  )
