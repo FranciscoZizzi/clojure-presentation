e1420ff2758df4fcce4d0a689f748a27127ebcc6
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-12-01 19:59:54 -0300

    add slides

--- notebooks/slideshow.clj
+++ notebooks/slideshow.clj
@@ -3,13 +3,12 @@
 	(:require [nextjournal.clerk :as clerk]
 			  [nextjournal.clerk-slideshow :as slideshow])
 	)
-{::clerk/visibility {:code :hide :result :hide}}
-(clerk/add-viewers! [slideshow/viewer])
 
 {::clerk/visibility {:code :hide :result :show}}
 (clerk/html [:div.flex.items-center.overflow-hidden.gap-4
 			 [:img.h-12.my-0 {:src "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Clojure_logo.svg/800px-Clojure_logo.svg.png"}]
 			 [:h1 "Clojure"]])
+(clerk/html [:h4.absolute.top-60 "Francisco Zizzi"])
 
 ;; ---
 ;; # Brief History
@@ -106,22 +105,26 @@
 	[1 2 3])
 
 ;; ---
-;; # def and defn
-;; _def_ is used to assign a name to a value
+;; # def and let
+;; _def_ is used to assign a name to a value and adds it to the global context
 (def my-clojure-vector [1 2 3 4])
 (str "This is MY vector -> " my-clojure-vector) ; str concatenates strings, also turns values into strings automatically
-;; Functions are values, of course
-(def plus +)
-(plus 1 2)
-;; _defn_ is used to declare functions
+;; _let_ is also used to assign a name to a value but just in a local context, only available within the parentheses of _let_
+(let [a 5 b 6 c 2]
+	[a b c])
+;; Or also:
+(let [[a b c] [1 2 (reduce + (take 10 (range)))]]
+	[a b c])
+
+;; ---
+;; # Functions
+;; We can create a function with _defn_
 {::clerk/visibility {:result :hide}}
 (defn my-clojure-function "Optional docstring" [arg1 arg2]
 	(str arg1 arg2))
 {::clerk/visibility {:result :show}}
 (my-clojure-function "Hello " "world!")
 
-;; ---
-;; # Functions
 ;; It is possible to define functions with an undefined amount of arguments
 {::clerk/visibility {:result :hide}}
 (defn average [& args]
@@ -138,6 +141,39 @@
 	)
 {::clerk/visibility {:result :show}}
 [(multi-arity 1) (multi-arity 1 2) (multi-arity 1 2 3)]
+
+;; ---
+;; # Higher Order Functions
+;; ### Function Literals
+;; We can define anonymous functions using _fn_
+{::clerk/visibility {:result :hide}}
+(def bands [
+			   {:name "Brown Beaters"   :genre :rock}
+			   {:name "Sunday Sunshine" :genre :blues}
+			   {:name "Foolish Beaters" :genre :rock}
+			   {:name "Monday Blues"    :genre :blues}
+			   {:name "Friday Fewer"    :genre :blues}
+			   {:name "Saturday Stars"  :genre :jazz}
+			   {:name "Sunday Brunch"   :genre :jazz}
+			   ])
+{::clerk/visibility {:result :show}}
+(def rock-bands
+	(filter
+	 (fn [band] (= :rock (:genre band)))
+	 bands))
+;; Or we can use #. % represents an argument, in this case a band
+(def rock-bands (filter #(= :rock (:genre %)) bands))
+;; It supports multiple arguments:
+(#(str %1 %2 %3) 1 2 3)
+;; ### Functions returning functions and functions as values
+;; Adder returns a function which we use to define add-five
+{::clerk/visibility {:result :hide}}
+(defn adder [x]
+	(fn [a] (+ x a)))
+(def add-five (adder 5))
+
+{::clerk/visibility {:result :show}}
+(add-five 100)
 
 ;; ---
 ;; # Destructuring
@@ -162,7 +198,7 @@
 ;; TODO
 
 ;; ---
-;; # Let
+;; # Macros
 ;; TODO
 
 ;; ---
